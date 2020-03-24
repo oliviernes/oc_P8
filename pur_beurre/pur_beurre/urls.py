29ff19b830bf4d69ec16c7df4b9106b30e294475
Olivier71 <olivier.nesme@gmail.com>
2020-03-24 09:02:43 +0100

    Change url patterns of the Pur Beurre website

--- pur_beurre/pur_beurre/urls.py
+++ pur_beurre/pur_beurre/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('food_substitute/', include('food_substitute.urls')),
+    path('', include('food_substitute.urls')),
     path('admin/', admin.site.urls),
 ]
