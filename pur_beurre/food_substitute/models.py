acb47c045e69cabfd5e66aaa0e17516f3d8a79b9
Olivier71 <olivier.nesme@gmail.com>
2020-03-28 11:42:46 +0100

    Add models category and products #6

--- pur_beurre/food_substitute/models.py
+++ pur_beurre/food_substitute/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class category(models.Model):
+    name=models.CharField(max_length=150)
+
+class products(models.Model):
+    code = models.CharField(max_length=13)
+    category_id=models.ForeignKey(category, on_delete=models.CASCADE)
+    
+
