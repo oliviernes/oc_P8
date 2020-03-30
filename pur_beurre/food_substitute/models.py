a29fb97eec9a211e84138c9ee2567aaf26c24ac0
Olivier71 <olivier.nesme@gmail.com>
2020-03-30 19:24:46 +0200

    Add welcome view #7

--- pur_beurre/food_substitute/models.py
+++ pur_beurre/food_substitute/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 
-class category(models.Model):
+class Category(models.Model):
     name=models.CharField(max_length=150)
 
-class products(models.Model):
+class Products(models.Model):
     code = models.CharField(max_length=13)
-    category=models.ForeignKey(category, on_delete=models.CASCADE)
+    category=models.ForeignKey(Category, on_delete=models.CASCADE)
--- pur_beurre/food_substitute/views.py
+++ pur_beurre/food_substitute/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Category, Products
 
 # Create your views here.
 
 
+def welcome(request):
+    return render(request, "food_substitute/welcome.html")
 
 class DetailView(generic.DetailView):
     model=Products
