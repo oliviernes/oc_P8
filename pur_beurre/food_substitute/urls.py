dee482a41c3599cd679c322c1981f1d483ed6b92
Olivier71 <olivier.nesme@gmail.com>
2020-04-01 20:24:23 +0200

    Add detail view and url #7

--- pur_beurre/food_substitute/urls.py
+++ pur_beurre/food_substitute/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.welcome, name="welcome"),
+    path("product/<code>", views.detail, name="detail"),
     path("disclaimer/", views.disclaimer, name="disclaimer"),
 ]
--- pur_beurre/food_substitute/views.py
+++ pur_beurre/food_substitute/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Category, Products
+from .API import product_data
 
 # Create your views here.
-
 
 def welcome(request):
     return render(request, "food_substitute/welcome.html")
 
-class DetailView(generic.DetailView):
-    model=Products
-    template_name='food_substitute/detail.html'
+#~ class DetailView(generic.DetailView):
+    #~ model=Products
+    #~ template_name='food_substitute/detail.html'
 
+def detail(request, code):
+    context=product_data(code)
+    print(context)
+    return render(request, 'food_substitute/detail.html', context)
+    
 def disclaimer(request):
     return render(request, "food_substitute/disclaimer.html")
