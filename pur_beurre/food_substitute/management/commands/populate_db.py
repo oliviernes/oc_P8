6883dd0432f308383345f05cd0d4af016d83b518
Olivier71 <olivier.nesme@gmail.com>
2020-04-18 15:10:43 +0200

    Add an if statements in populate_db #9
    To not duplicate products entries in the db

--- pur_beurre/food_substitute/management/commands/populate_db.py
+++ pur_beurre/food_substitute/management/commands/populate_db.py
@@ -7,7 +7,7 @@ class Command(BaseCommand):
     help = "Populate the db with products' data using the Openfoodfacts API"
 
     def search_data(self, category):
-    
+
         "Search product's data for a given category over the API openfoodfacts"
         
         search_param = {
@@ -40,17 +40,22 @@ DB")
             if len(infos_prod)>0:
                 for i in range(250):
                     prod_index=infos_prod[i]
-                    prod=Products(code=prod_index['code'], \
-    category=Category.objects.get(id=cat.id),)
-                    prod.name=prod_index.get("product_name_fr", "")
-                    prod.image_front=prod_index.get('image_front_url', "")
-                    prod.image_front_thumb=prod_index.get('image_front_thumb_url', "")
-                    prod.image_nutrition=prod_index.get('image_nutrition_url', "")
-                    prod.nutrition_grades=prod_index.get('nutrition_grades', "")
-                    prod.url=prod_index.get('url', "")
-                    prod.save()
-                    print(f"The product {prod.name} has been inserted in \
+                    if len(Products.objects.filter(code=prod_index['code']))==0:
+                        prod=Products(code=prod_index['code'])
+                        prod.name=prod_index.get("product_name_fr", "")
+                        prod.image_front=prod_index.get('image_front_url', "")
+                        prod.image_front_thumb=prod_index.get('image_front_thumb_url', "")
+                        prod.image_nutrition=prod_index.get('image_nutrition_url', "")
+                        prod.nutrition_grades=prod_index.get('nutrition_grades', "")
+                        prod.url=prod_index.get('url', "")
+                        prod.save()
+                        prod.category.add(Category.objects.get(id=cat.id))
+                        prod.save()
+                        print(f"The product {prod.name} has been inserted in \
 the DB")
+                    else:
+                        prod=Products.objects.get(code=prod_index['code'])
+                        prod.category.add(Category.objects.get(id=cat.id))
             else:
                 print(f"The category {cat.name} is not present in OFF\
 API. No products will be inserted in the database")
