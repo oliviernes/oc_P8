e165740ef0a438b692611448c45923549293afa1
Olivier71 <olivier.nesme@gmail.com>
2020-04-19 14:00:16 +0200

    Add docstring in test_populate.db

--- pur_beurre/pur_beurre/tests/test_populate.py
+++ pur_beurre/pur_beurre/tests/test_populate.py
@@ -5,7 +5,11 @@ from food_substitute.models import Products
 
 @mark.django_db
 def test_duplicate_products():
+    """Test if the database have duplicate products"""
+
     products_db=Products.objects.all()
     code_list=[products_db[i].code for i in range(len(products_db))]
     code_unique_list=set(code_list)
+
     assert len(code_list)==len(code_unique_list)
+
