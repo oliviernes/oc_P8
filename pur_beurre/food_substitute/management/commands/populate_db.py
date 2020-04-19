e48cd7b1f91a823701cf666ebc3494aadac8ea18
Olivier71 <olivier.nesme@gmail.com>
2020-04-19 14:33:38 +0200

    Fix error displaying inserted categories not in OFF #9

--- pur_beurre/food_substitute/management/commands/populate_db.py
+++ pur_beurre/food_substitute/management/commands/populate_db.py
@@ -3,13 +3,14 @@ from food_substitute.models import Category, Products
 from food_substitute.config import CATEGORIES
 import requests
 
+
 class Command(BaseCommand):
     help = "Populate the db with products' data using the Openfoodfacts API"
 
     def search_data(self, category):
 
         "Search product's data for a given category over the API openfoodfacts"
-        
+
         search_param = {
             "search_terms": category,
             "search_tag": "categories_tag",
@@ -25,37 +26,44 @@ class Command(BaseCommand):
         # output of request as a json file
         req_output = req.json()
 
-        prod_cat=req_output['products']
+        prod_cat = req_output["products"]
 
         return prod_cat
-    
+
     def handle(self, *args, **options):
-        
+
         for categor in CATEGORIES:
-            infos_prod=self.search_data(categor)
-            cat=Category(name=categor)
-            cat.save()
-            print(f"The category {cat.name} has been insterted in the \
-DB")
-            if len(infos_prod)>0:
+            infos_prod = self.search_data(categor)
+            cat = Category(name=categor)
+
+            if len(infos_prod) > 0:
+                cat.save()
+                print(f"The category {cat.name} has been insterted in the DB")    
                 for i in range(250):
-                    prod_index=infos_prod[i]
-                    if len(Products.objects.filter(code=prod_index['code']))==0:
-                        prod=Products(code=prod_index['code'])
-                        prod.name=prod_index.get("product_name_fr", "")
-                        prod.image_front=prod_index.get('image_front_url', "")
-                        prod.image_front_thumb=prod_index.get('image_front_thumb_url', "")
-                        prod.image_nutrition=prod_index.get('image_nutrition_url', "")
-                        prod.nutrition_grades=prod_index.get('nutrition_grades', "")
-                        prod.url=prod_index.get('url', "")
+                    prod_index = infos_prod[i]
+                    """Add an if statements to not duplicate products entry in the db"""
+                    if len(Products.objects.filter(code=prod_index["code"])) == 0:
+                        prod = Products(code=prod_index["code"])
+                        prod.name = prod_index.get("product_name_fr", "")
+                        prod.image_front = prod_index.get("image_front_url", "")
+                        prod.image_front_thumb = prod_index.get(
+                            "image_front_thumb_url", ""
+                        )
+                        prod.image_nutrition = prod_index.get("image_nutrition_url", "")
+                        prod.nutrition_grades = prod_index.get("nutrition_grades", "")
+                        prod.url = prod_index.get("url", "")
                         prod.save()
                         prod.category.add(Category.objects.get(id=cat.id))
                         prod.save()
-                        print(f"The product {prod.name} has been inserted in \
-the DB")
+                        print(
+                            f"The product {prod.name} has been inserted in \
+the DB"
+                        )
                     else:
-                        prod=Products.objects.get(code=prod_index['code'])
+                        prod = Products.objects.get(code=prod_index["code"])
                         prod.category.add(Category.objects.get(id=cat.id))
             else:
-                print(f"The category {cat.name} is not present in OFF\
-API. No products will be inserted in the database")
+                print(
+                    f"The category {cat.name} is not present in OFF\
+API. No products will be inserted in the database"
+                )
