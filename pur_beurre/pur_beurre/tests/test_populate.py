c2c4efd541e965fdbe24aa25dc5bee7d27fc5968
Olivier71 <olivier.nesme@gmail.com>
2020-04-26 15:25:51 +0200

    Add a test to check if a product with missing fields can populate the db #11

--- pur_beurre/pur_beurre/tests/test_populate.py
+++ pur_beurre/pur_beurre/tests/test_populate.py
@@ -1,8 +1,25 @@
+import json
+
 from pytest import fixture, mark
 
 from food_substitute.management.commands.populate_db import Command
 from food_substitute.models import Products
 
+@fixture
+def nutella():
+    """Mock API response of nutella products"""
+    
+    nutella_file="pur_beurre/tests/nutella.json"
+    
+    with open(nutella_file, "r") as json_file:
+        json_response_from_api = json.load(json_file)
+    
+    return json_response_from_api
+
+@fixture
+def db_feed():
+    return Command()
+   
 @mark.django_db
 def test_duplicate_products():
     """Test if the database have duplicate products"""
@@ -13,3 +30,16 @@ def test_duplicate_products():
 
     assert len(code_list)==len(code_unique_list)
 
+@mark.django_db
+def test_fields_missing(db_feed, nutella):
+    
+    category="pâtes à tartiner au chocolat"
+    Fake_API_response=[nutella[0]['fields']]
+    
+    db_feed.populate(Fake_API_response, category)
+    prod=Products.objects.all()[0]
+    
+    assert prod.code == "3017620422003"
+    assert prod.name == ""
+    assert prod.url == ""
+    assert prod.image_small ==""    
