7327aebd1974ca41e5e7dbe15def83b1694e3638
Olivier71 <olivier.nesme@gmail.com>
2020-09-21 12:52:26 +0200

    raises Http404 instead of the model’s DoesNotExist exception

--- pur_beurre/food_substitute/views.py
+++ pur_beurre/food_substitute/views.py
@@ -1,15 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 from .models import Category, Products
 
+import pdb
 
 def welcome(request):
     return render(request, "food_substitute/welcome.html")
 
 
 def detail(request, code):
-    product = Products.objects.get(code=code)
+    # product = Products.objects.get(code=code)
+    product = get_object_or_404(Products, code=code)
 
     context = {"product": product}
     return render(request, "food_substitute/detail.html", context)
