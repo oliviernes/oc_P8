66b4dfd301369a2a78c7e57817b329ac40f849fe
Olivier71 <olivier.nesme@gmail.com>
2020-09-26 22:17:38 +0200

    Move folder functional_tests and add __init__.py

--- pur_beurre/functional_tests/tests.py
+++ pur_beurre/functional_tests/tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -19,14 +21,37 @@ class NewVisitorTest(unittest.TestCase):
         # She notices the form bar in the header and in the center of the page
 
         self.assertIn('Pur', self.browser.title)
-        self.fail('Finish the test!')
+        bar_text = self.browser.find_element_by_tag_name('a').text
+        self.assertIn('Pur', bar_text)
 
         # She is invited to enter a product item straight away
 
+        inputbox = self.browser.find_element_by_name('query') 
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Chercher'
+        )
+
         # She types "Nutella" into the text box in the middle of the screen
+
+        inputboxs = self.browser.find_elements_by_name('query') 
+        inputbox_center = inputboxs[1]
+        self.assertEqual(
+            inputbox_center.get_attribute('placeholder'),
+            'Produit'
+        )
+        inputbox_center.send_keys('Nutella')
 
         # When she hits enter, the page updates, and now the page lists
         # healthier products of the same category.
+
+        inputbox_center.send_keys(Keys.ENTER)
+        time.sleep(5)
+
+        prod_text = self.browser.find_element_by_tag_name('h4').text
+        self.assertIn('Protein', prod_text)
+
+        self.fail('Finish the test!')
 
         # She selects a product and get a new page with the detail of the product
 
