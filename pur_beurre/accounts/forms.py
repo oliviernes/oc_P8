395b17c14b1113279e396a368cec30600cfcae0b
Olivier71 <olivier.nesme@gmail.com>
2020-10-03 15:04:53 +0200

    Add link signup to login.html

--- pur_beurre/accounts/forms.py
+++ pur_beurre/accounts/forms.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(label='Prénom', max_length=30, required=True, help_text='Demandé.')
+    first_name = forms.CharField(label='Prénom', max_length=30, required=True, help_text='Requis.')
     last_name = forms.CharField(label='Nom', max_length=30, required=False, help_text='Optionel.')
-    email = forms.EmailField(label='Courriel', max_length=254, help_text='Demandé. Entrez une adresse email valide.')
+    email = forms.EmailField(label='Courriel', max_length=254, help_text='Requis. Entrez une adresse email valide.')
 
     class Meta:
         model = User
