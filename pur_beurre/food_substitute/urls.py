13cf5d051b7936dfa282a5a01ca4ea2eae7418ea
Olivier71 <olivier.nesme@gmail.com>
2020-10-05 16:27:53 +0200

    Add favorites.html page to record user's substitutes

--- pur_beurre/food_substitute/urls.py
+++ pur_beurre/food_substitute/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("product/<code>", views.detail, name="detail"),
     path("disclaimer/", views.disclaimer, name="disclaimer"),
     path("search/", views.search, name="search"),
+    path("favorites/", views.favorites, name="favorites"),
+    path("save/<produc>/<substitut>", views.save, name="save"),
 ]
