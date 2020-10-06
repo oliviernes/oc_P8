606822673f9abc1bbf8109ca688faa3de3b14386
Olivier71 <olivier.nesme@gmail.com>
2020-10-06 15:06:31 +0200

    Move the if user.is_authenticated statement from frontend to backend

--- pur_beurre/food_substitute/views.py
+++ pur_beurre/food_substitute/views.py
@@ -1,6 +1,6 @@
 from collections import Counter
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.contrib.auth.models import User
 
@@ -88,32 +88,35 @@ of the product selected"""
     return render(request, "food_substitute/category.html", context)
 
 def save(request, produc, substitut):
-    user_id = request.user.id
-    user = User.objects.get(id = user_id)
-    product = Products.objects.get(code = produc)
-    substitute = Products.objects.get(code = substitut)
-    duplicates = False
-    recorded = Favorites.objects.filter(users = user)
-    favorite_recorded=[]
-    for record in recorded:
-        rec = (Products.objects.get(id = record.products_id), Products.objects.get(id = record.substitute_id))
-        favorite_recorded.append(rec)
-    if len(Favorites.objects.filter(users = user, products=product, substitute=substitute)) > 0:
-        duplicates = True
-    else:    
-        record = Favorites(users = user, products=product, substitute=substitute)
-        record.save()
-    recording = True
-    context = {
-        "recording": recording,
-        "user": user,
-        "product": product,
-        "substitute": substitute,
-        "duplicates": duplicates,
-        "favorite_recorded": favorite_recorded,
-    }
 
-    return render(request, "food_substitute/favorites.html", context)
+    if request.user.is_authenticated:
+        user_id = request.user.id
+        user = User.objects.get(id = user_id)
+        product = Products.objects.get(code = produc)
+        substitute = Products.objects.get(code = substitut)
+        duplicates = False
+        recorded = Favorites.objects.filter(users = user)
+        favorite_recorded=[]
+        for record in recorded:
+            rec = (Products.objects.get(id = record.products_id), Products.objects.get(id = record.substitute_id))
+            favorite_recorded.append(rec)
+        if len(Favorites.objects.filter(users = user, products=product, substitute=substitute)) > 0:
+            duplicates = True
+        else:
+            record = Favorites(users = user, products=product, substitute=substitute)
+            record.save()
+        recording = True
+        context = {
+            "recording": recording,
+            "user": user,
+            "product": product,
+            "substitute": substitute,
+            "duplicates": duplicates,
+            "favorite_recorded": favorite_recorded,
+        }
+        return render(request, "food_substitute/favorites.html", context)
+    else:
+        return redirect("login")
 
 def favorites(request):
     user_id = request.user.id
