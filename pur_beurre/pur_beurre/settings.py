4180dd1291b23c092710cd110020ac93dc11e835
Olivier71 <olivier.nesme@gmail.com>
2020-10-16 18:05:16 +0200

    Add DATABASE config for production

--- pur_beurre/pur_beurre/settings.py
+++ pur_beurre/pur_beurre/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -87,16 +89,20 @@ WSGI_APPLICATION = "pur_beurre.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "pur_beurre",
-        "USER": "olivierx",
-        "PASSWORD": "1979",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+if os.environ.get('ENV') == 'PRODUCTIONXXX':
+    db_from_env = dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(db_from_env)
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "pur_beurre",
+            "USER": "olivierx",
+            "PASSWORD": "1979",
+            "HOST": "127.0.0.1",
+            "PORT": "5432",
+        }
     }
-}
 
 
 # Password validation
