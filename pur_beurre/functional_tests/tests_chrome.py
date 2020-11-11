e47306346265852d93fa98ad6c412613a37ffed7
Olivier71 <olivier.nesme@gmail.com>
2020-11-11 21:30:01 +0100

    Add window size option in chromedriver

--- pur_beurre/functional_tests/tests_chrome.py
+++ pur_beurre/functional_tests/tests_chrome.py
@@ -18,6 +18,7 @@ class NewVisitorTest(LiveServerTestCase):
         options.add_argument('--headless')
         options.add_argument('--disable-gpu')
         options.add_argument('--no-sandbox')
+        options.add_argument('--window-size=1420,1080')
 
         self.browser = webdriver.Chrome(chrome_options=options)
 
