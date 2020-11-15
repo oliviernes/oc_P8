b4b7d39b9fcfd1da800300a852d1eceb40a063cd
Olivier71 <olivier.nesme@gmail.com>
2020-11-15 09:29:15 +0100

    Remove useless comment in tests.py

--- pur_beurre/functional_tests/tests.py
+++ pur_beurre/functional_tests/tests.py
@@ -6,7 +6,6 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 from selenium.webdriver.firefox.options import Options
-# from selenium.webdriver import Firefox
 
 
 class NewVisitorTest(LiveServerTestCase):
